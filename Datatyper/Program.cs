d8f4b4d749538b98e270b2a715828d4c346bf2b1
Abdirahman Dayib <Abdirahman.Dayib@ga.ntig.se>
2021-08-24 08:49:52 +0200

    Add project files.

---
+++ Datatyper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Datatyper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //01-Utskrift
+
+            //Syntax // Kommentar // Variabler 
+
+            // All kod börjar inom dessa {} aka måsvingar 
+
+            //För att skriva något till konsolen när vi kör programmet 
+            //Använder vi denna syntax nedanför
+           
+            Console.WriteLine("Hejsan, jag börjar greppa C#");
+
+            // Uppgift 1: Skriv ut "Hejsan, jag heter "Dittnamn" och Jag börjar greppa C#" tre gånger; 
+
+
+
+
+
+        }
+    }
+}
