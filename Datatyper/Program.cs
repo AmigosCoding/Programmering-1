b2cf0b4591bac1967e2af315bf2d1727396ea18c
Abdirahman Dayib <Abdirahman.Dayib@ga.ntig.se>
2021-08-24 08:52:56 +0200

    Added my name

--- Datatyper/Program.cs
+++ Datatyper/Program.cs
@@ -15,7 +15,7 @@ namespace Datatyper
             //För att skriva något till konsolen när vi kör programmet 
             //Använder vi denna syntax nedanför
            
-            Console.WriteLine("Hejsan, jag börjar greppa C#");
+            Console.WriteLine("Hejsan, jag börjar greppa C#!!!! Abdi");
 
             // Uppgift 1: Skriv ut "Hejsan, jag heter "Dittnamn" och Jag börjar greppa C#" tre gånger; 
 
